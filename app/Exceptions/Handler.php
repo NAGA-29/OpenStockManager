18f92cd8ff485cdf125bca48d1d20dfe32994dbb
naga <n.work19930418@gmail.com>
2026-03-17 23:55:00 +0900

    fix: Sentryに例外を送信する際のコメントを追加

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,14 +2,16 @@
 
 namespace App\Exceptions;
 
+use Throwable;
 use App\Exceptions\Domain\DeviceException;
 use App\Exceptions\Infrastructure\CsvImportException;
 use App\Exceptions\Infrastructure\ImageProcessingException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Session\TokenMismatchException;
+// Facade
 use Illuminate\Support\Facades\Log;
+// Sentry
 use Sentry\Laravel\Integration;
-use Throwable;
 
 class Handler extends ExceptionHandler
 {
@@ -41,7 +43,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            Integration::captureUnhandledException($e);
+            Integration::captureUnhandledException($e); // Sentryに例外を送信
         });
     }
 
