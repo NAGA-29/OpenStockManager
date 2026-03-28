40f14e166957e299129eb0b6731cafc3ac99bb0b
naga <n.work19930418@gmail.com>
2026-03-29 01:42:06 +0900

    fix: ステータス説明の文言を簡潔に修正

--- resources/views/devices/components/status_legend.blade.php
+++ resources/views/devices/components/status_legend.blade.php
@@ -9,10 +9,10 @@
             <span class="status-legend__item"><i class="fas fa-images"></i> 写真アリ</span>
         </div>
         <div class="status-legend__row">
-            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--success"></span> 貸出可能 - 即座に貸し出せる</span>
-            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--primary"></span> 貸出中 - レンタル中</span>
+            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--success"></span> 貸出可能</span>
+            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--primary"></span> 貸出中</span>
             <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--danger"></span> 不具合 - 動作に問題あり</span>
-            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--warning"></span> 修理中 - 修理待ち/修理中</span>
+            <span class="status-legend__item"><span class="status-legend__dot status-legend__dot--warning"></span> 修理中 - 修理待ち中</span>
         </div>
     </div>
 </details>
